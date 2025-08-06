f4aec205919306213f6e3402ea30412351b91fcb
Asitha Dissanayaka <adissanayaka470@gmail.com>
2025-08-06 18:26:14 +0530

    Update chatbot.php

--- components/chatbot.php
+++ components/chatbot.php
@@ -82,7 +82,7 @@
 
             loadingIndicator.style.display = 'flex'; 
 
-            const apiKey = "AIzaSyA9QLJU0sqsXUzZXSFsWztl_B2TgfaJbMw";
+            const apiKey = "...Gemini Api Key";
             const apiUrl = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-preview-05-20:generateContent?key=${apiKey}`;
 
             let retries = 0;
